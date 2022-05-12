a59710f22c72c28c0d2781ce6e79b11382016f20
Ferguson Shuai <ferguson_shuai@hotmail.com>
2022-05-11 17:11:43 -0700

    calculateEnergyMap function done.

--- src/contentAwareResize.ts
+++ src/contentAwareResize.ts
@@ -64,3 +64,62 @@ export const ResizeImageWidth = (
 
     return {img, size};
 }
+
+// Now we are trying to calculate EnergyMap.
+const calculateEnergyMap = (img: ImageData, {w, h}: ImageSize): EnergyMap => {
+    // Create an empty energy map where each pixel has infinitely high energy.
+    const energyMap: number[][] = matrix<number>(w, h, Infinity);
+    // Then update energy for each pixel.
+    for (let y = 0; y < h; y++) {
+        for (let x = 0; x < w; x++) {
+            const leftPixel = (x - 1) >= 0 ? getPixel(img, {x: x-1, y}) : null;
+            const middlePixel = getPixel(img, {x,y});
+            const rightPixel = (x + 1) <= 0 ? getPixel(img, {x: x+1, y}) : null;
+            energyMap[y][x] = getPixelEnergy(leftPixel, middlePixel, rightPixel);
+        }
+    }
+    return energyMap;
+}
+
+// A helper function that generates a 2D array prefilled with generic type data.
+const matrix = <T>(w: number, h: number, filler: T): T[][] => {
+    return new Array(h).fill(null).map(()=>{
+        return new Array(w).fill(filler);
+    });
+};
+
+// Can have a helper function to calculate the energy of a pixel.
+const getPixelEnergy = (left: Color | null, middle: Color, right: Color | null): number => {
+    // Middle pixel is the pixel we're calculating the energy for.
+    const [mR, mG, mB] = middle;
+
+    // Energy contributed by left pixel.
+    let lEnergy = 0;
+    if (left) {
+        const [lR, lG, lB] = left;
+        lEnergy = (lR - mR) ** 2 + (lG - mG) ** 2 + (lB - mB) ** 2;
+    }
+
+    // Energy contributed by right pixel.
+    let rEnergy = 0;
+    if (left) {
+        const [rR, rG, rB] = right;
+        rEnergy = (rR - mR) ** 2 + (rG - mG) ** 2 + (rB - mB) ** 2;
+    }
+
+    return Math.sqrt(lEnergy + rEnergy);
+}
+
+// Helper function that returns the color of a specified pixel from ImageData
+const getPixel = (img: ImageData, {x, y}: Coordinate): Color => {
+    // The ImageData.data is a flat 1D array, every 4 consecutive numbers represent a
+    // pixel, eg, [r0, g0, b0, a0, r1, g1, b1, a1,...]
+    // Thus need to convert {x,y} coordinate into linear index.
+    const i = y * img.width + x;
+    const cellsPerColor = 4;  // RGBA.
+
+    return img.data.subarray(i * cellsPerColor, i * cellsPerColor + cellsPerColor);
+}
+
+
+
