1d12ce0b332ee9600fb1b173b9f305223b2e4c63
Ferguson Shuai <ferguson_shuai@hotmail.com>
2022-05-13 13:03:58 -0700

    findLowEnergySeam function done.

--- src/contentAwareResize.ts
+++ src/contentAwareResize.ts
@@ -74,7 +74,7 @@ const calculateEnergyMap = (img: ImageData, {w, h}: ImageSize): EnergyMap => {
         for (let x = 0; x < w; x++) {
             const leftPixel = (x - 1) >= 0 ? getPixel(img, {x: x-1, y}) : null;
             const middlePixel = getPixel(img, {x,y});
-            const rightPixel = (x + 1) <= 0 ? getPixel(img, {x: x+1, y}) : null;
+            const rightPixel = (x + 1) < w ? getPixel(img, {x: x+1, y}) : null;
             energyMap[y][x] = getPixelEnergy(leftPixel, middlePixel, rightPixel);
         }
     }
@@ -122,4 +122,80 @@ const getPixel = (img: ImageData, {x, y}: Coordinate): Color => {
 }
 
 
+/*************************findLowEnergySeam********************** */
+// The metadata for the pixels in the seam.
+type SeamPixelMeta = {
+    energy: number,  // The energy of the pixel.
+    coordinate: Coordinate,  // The coordinate of the pixel.
+    previous: Coordinate | null,  // The previous pixel in a seam. (can be used to retrieve the seam)
+}
+
+// Finds the seam (the sequence of pixels from top to bottom) that has the
+// lowest resulting energy using bottom-up Dynamic Programming approach.
+const findLowEnergySeam(energyMap: EnergyMap, {w, h}: ImageSize): Seam => {
+    // need a 2D array of SeamPixelMeta to store the lowest energy for each pixel, and its prev pixel.
+    const seamEnergies: (SeamPixelMeta | null)[][] = matrix<SeamPixelMeta | null>(w, h, null);
+
+    // Populate the first row of the map by just copying the energy map.
+    for (let col = 0; col < w; col++) {
+        seamEnergies[0][col] = {
+            energy: energyMap[0][col],
+            coordinate: {x: 0, y: col},
+            previous: null,
+        };
+    }
+
+    // Populate the rest of the map.
+    for (let y = 1; y < h; y++) {
+        for (let x = 0; x < w; x++) {
+            let minPrevEnergy: number = Infinity;
+            let minPrevX: number = x;
+            for (let i = (x - 1); i <= (x + 1); i++) {
+                if (i >= 0 && i < w && seamEnergies[y-1][i].energy < minPrevEnergy) {
+                    minPrevEnergy = seamEnergies[y-1][i].energy;
+                    minPrevX = i;
+                }
+            }
+
+            seamEnergies[y][x] = {
+                energy: minPrevEnergy + energyMap[y][x],
+                coordinate: {x, y},
+                previous: {x: minPrevX, y: y-1},
+            };
+        }
+    }
+
+    // Find the lowest energy seam.
+    // where is it on the last row?
+    let lastMinCoordinate: Coordinate | null = null;
+    let minSeamEnergy = Infinity;
+    for (let x = 0; x < w; x++) {
+        const y = h - 1;
+        if (seamEnergies[y][x].energy < minSeamEnergy) {
+            minSeamEnergy = seamEnergies[y][x].energy;
+            lastMinCoordinate = {x, y};
+        }
+    }
+
+    // Backtracking from the lowest energy pixel on last row.
+    const seam: Seam = [];  // Initialize an empty array
+    if (!lastMinCoordinate) {
+        return seam;
+    }
+
+    const {x: lastMinX, y: lastMinY} = lastMinCoordinate;
+
+    let currentSeam = seamEnergies[lastMinY][lastMinX];
+    while (currentSeam) {
+        seam.push(currentSeam.coordinate);
+        const prevMinCoordinates = currentSeam.previous;
+        if (!prevMinCoordinates) {
+            currentSeam = null;
+        } else {
+            currentSeam = seamEnergies[prevMinCoordinates.y][prevMinCoordinates.x];
+        }
+    }
+
+    return seam;
+}
 
